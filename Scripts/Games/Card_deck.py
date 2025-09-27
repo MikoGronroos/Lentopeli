3ca4392819ba308faf6f6d50cd7200aef9d96226
irevapa <irene.vapalahti@outlook.com>
2025-09-27 14:01:47 +0300

    card deck

---
+++ Scripts/Games/Card_deck.py
@@ -0,0 +1,13 @@
+import random
+
+def getCards():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    deck = [(rank, suit) for suit in suits for rank in ranks]
+    return deck
+
+def Shuffle(deck):
+    random.shuffle(deck)
+    return deck
+
