262033f9d3a01e22bac8c2178715ebeb11cb31fd
irevapa <irene.vapalahti@outlook.com>
2025-09-27 14:55:51 +0300

    High low card added

--- Scripts/Games/Card_deck.py
+++ Scripts/Games/Card_deck.py
@@ -2,7 +2,7 @@ import random
 
 def getCards():
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
 
     deck = [(rank, suit) for suit in suits for rank in ranks]
     return deck
---
+++ Scripts/Games/HighCard_LowCard.py
@@ -0,0 +1,23 @@
+import Card_deck as deck
+def Game():
+    cards = deck.getCards()
+    cards = deck.Shuffle(cards)
+    playerCard = cards.pop(0)
+    dealerCard = cards.pop(0)
+    moneyGambled = input("Determine how much you want to lose: ")
+    print("Welcome to High card, Low card!")
+    print (f"Your card:{playerCard}")
+    playerGuessedCorrectly = False
+    answer = input("Higher, Lower or Same?")
+    if answer == "Higher" and playerCard > dealerCard:
+        playerGuessedCorrectly = True
+    elif answer == "Lower" and playerCard < dealerCard:
+        playerGuessedCorrectly = True
+    elif answer == "Same" and playerCard == dealerCard:
+        playerGuessedCorrectly = True
+
+    if playerGuessedCorrectly:
+        print("You guessed correctly.")
+    else:
+        print("You guessed incorrectly.")
+    return
