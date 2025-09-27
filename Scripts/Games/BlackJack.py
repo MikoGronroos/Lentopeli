31cd2996244eba3f00bdbe383ad4f4bf1cd853a5
Sofia Ravi <fiialina@outlook.com>
2025-09-27 16:54:05 +0300

    Added working blackjack

---
+++ Scripts/Games/BlackJack.py
@@ -0,0 +1,75 @@
+import Card_deck as deck
+
+def Game():
+    print("Howdy. Down for a round of blackjack?")
+    print("Dealer must stand at 17, ace can be either 11 or 1.")
+
+    cards = deck.getBlackjackCards()
+    cards = deck.Shuffle(cards)
+
+    playerCards = []
+    playerCards.append(cards.pop(0))
+    playerCards.append(cards.pop(0))
+
+    dealerCards = []
+    dealerCards.append(cards.pop(0))
+    dealerCards.append(cards.pop(0))
+
+    playerState = 0
+    gameOver = False
+
+    print(f"Your cards: {playerCards}, dealer's cards: {dealerCards[0]}")
+    while True:
+        if gameOver:
+            break
+        decision = input("Do you wish to hit or stand? hit/stand:")
+        if decision == "hit":
+            playerCards.append(cards.pop(0))
+            print(f"Your cards: {playerCards}")
+            if calculateValue(playerCards) > 21:
+                playerState = -1
+                break
+        elif decision == "stand":
+            while True:
+                dealerSum = calculateValue(dealerCards)
+                print(f"Dealer's cards: {dealerCards}")
+                if dealerSum <= 16:
+                    dealerCards.append(cards.pop(0))
+                elif dealerSum >= 17 and dealerSum <= 21:
+                    gameOver = True
+                    if calculateValue(dealerCards) > calculateValue(playerCards):
+                        playerState = -1
+                    elif calculateValue(dealerCards) == calculateValue(playerCards):
+                        playerState = 2
+                    elif calculateValue(dealerCards) < calculateValue(playerCards):
+                        playerState = 1
+                    break
+                elif dealerSum > 21:
+                    playerState = 1
+                    gameOver = True
+                    break
+
+    if playerState == -1:
+        print("You lost, I honestly expected better")
+    elif playerState == 1:
+        print("You won")
+    elif playerState == 2:
+        print("Tie")
+
+
+
+
+
+    return
+
+def calculateValue(cards):
+    newSum = sum(i for i, j in cards)
+    if newSum > 21:
+        newSum = 0
+        for i, j in cards:
+            if i == 11:
+                newSum += 1
+            else:
+                newSum += i
+    return newSum
+Game()
--- Scripts/Games/Card_deck.py
+++ Scripts/Games/Card_deck.py
@@ -2,7 +2,15 @@ import random
 
 def getCards():
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+    deck = [(rank, suit) for suit in suits for rank in ranks]
+    return deck
+
+
+def getBlackjackCards():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
     deck = [(rank, suit) for suit in suits for rank in ranks]
     return deck
