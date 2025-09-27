b4f410fff4744cf6a6096b8a89a298b27e542b5a
irevapa <irene.vapalahti@outlook.com>
2025-09-27 15:57:46 +0300

    Russion roulette added

---
+++ Scripts/Games/RussianRoulette.py
@@ -0,0 +1,24 @@
+import random
+def Game():
+    print("Welcome to the Russian Roulette!")
+    bullet = random.randint(1, 6)
+    player = 1
+    while True:
+        decision = input("Do you want to shoot yourself? (yes/no): ")
+        if decision == "yes":
+            if bullet == player:
+                print("You got shot.")
+                break
+            else:
+                print("Safe.")
+                player += 1
+        elif decision == "no":
+            print("You decited to stop.")
+            break
+        else:
+            print("Invalid input.")
+
+    print("The game is over.")
+    return
+
+Game()
