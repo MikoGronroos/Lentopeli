54ea3963be786259729d4a4aa5a6104de635a66f
Sofia Ravi <fiialina@outlook.com>
2025-09-30 13:47:31 +0300

    Add authentication. Login and Register

---
+++ Scripts/Core/authentications.py
@@ -0,0 +1,31 @@
+from ipaddress import summarize_address_range
+
+import mysql.connector
+import Scripts.Database.Database as db
+
+def Login():
+    while True:
+        characterName = input("Enter character name: ")
+        password = input("Enter password:")
+        if db.CheckPassword(characterName, password) == True:
+            print("Congratulations! You have successfully logged in! :)")
+            return True
+        else:
+            print("password or username incorrect :(")
+
+
+def Signup():
+    characterName = input("Enter character name: ")
+    while db.DoesPlayerExist(characterName) == True:
+        characterName = input("Character name already used. Pick a new name: ")
+    password = input("Enter password: ")
+    db.AddNewPlayer(characterName, password)
+    return True
+
+
+def Authenticate():
+    question = input("Log in or register a new character (login/register): ")
+    if question == "login":
+        return Login()
+    elif question == "register":
+        return Signup()
--- Scripts/Database/database.py
+++ Scripts/Database/database.py
@@ -1,17 +1,33 @@
 import mysql.connector
+import random
 
 yhteys = mysql.connector.connect(
         host='127.0.0.1',
         port=3306,
         database='flight_game',
         user='root',
-        password='123456789',
+        password='TiVi03DB',
         autocommit=True
         )
 
 kursori = yhteys.cursor()
 
 def TestiFunktio():
-    sql = f'select * from game';
-    kursori.execute(sql);
+    sql = f'select * from game'
+    kursori.execute(sql)
     return kursori.fetchall()
+
+def DoesPlayerExist(name):
+    sql = f'select * from game where screen_name = \'{name}\''
+    kursori.execute(sql)
+    return len(kursori.fetchall()) > 0
+
+def AddNewPlayer(name, password):
+    sql = f"INSERT INTO game (id, location, password, screen_name) VALUES (%s, %s, %s, %s)"
+    val = (random.randint(0,100000000), "00FA", password, name)
+    kursori.execute(sql, val)
+
+def CheckPassword(name, password):
+    sql = f'select * from game where screen_name = \'{name}\' and password = \'{password}\''
+    kursori.execute(sql)
+    return len(kursori.fetchall()) > 0
