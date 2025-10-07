39ec9aa19c8c548ade22051c1b7b3e9c522d84f8
Sofia Ravi <fiialina@outlook.com>
2025-10-07 10:42:46 +0300

    added rahasysteemi

--- Scripts/Database/database.py
+++ Scripts/Database/database.py
@@ -6,7 +6,7 @@ yhteys = mysql.connector.connect(
         port=3306,
         database='flight_game',
         user='root',
-        password='123456789',
+        password='TiVi03DB',
         autocommit=True
         )
 
@@ -30,4 +30,11 @@ def AddNewPlayer(name, password):
 def CheckPassword(name, password):
     sql = f'select * from game where screen_name = \'{name}\' and password = \'{password}\''
     kursori.execute(sql)
+    return len(kursori.fetchall()) > 0
+
+def UpdateMoney(name, money):
+    sql = f"UPDATE game set money = (money + {money}) where screen_name = \'{name}\'"
+    kursori.execute(sql)
+    sql2 = f"SELECT * FROM game where screen_name = \'{name}\' and money > 0"
+    kursori.execute(sql2)
     return len(kursori.fetchall()) > 0
