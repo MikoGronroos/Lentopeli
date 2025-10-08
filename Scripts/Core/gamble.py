c90e844f7a68ff4779e90b438567030283d237b9
Miko Gronroos <miko.gronroos@gmail.com>
2025-10-08 22:43:27 +0000

    Add functionality to gamble again if you want

--- Scripts/Core/gamble.py
+++ Scripts/Core/gamble.py
@@ -2,18 +2,24 @@ import Scripts.Database.database as db
 import Scripts.Core.account as account
 import Scripts.Games.blackJack as blackjack
 import Scripts.Games.russianRoulette as russianroulette
+import os
 
 
 games = {'blackjack': blackjack.Game,
          'russianroulette': russianroulette.Game}
 
 def gamble():
-    amountToGamble = int(input("How much would you like to gamble? "))
-    db.UpdateMoney(account.name, -amountToGamble)
-    selectedGame = 'blackjack'
-    game = games[selectedGame]
-    moneyWon = game(amountToGamble)
+    while True:
+        amountToGamble = int(input("How much would you like to gamble? "))
+        db.UpdateMoney(account.name, -amountToGamble)
+        selectedGame = 'blackjack'
+        game = games[selectedGame]
+        moneyWon = game(amountToGamble)
 
-    db.UpdateMoney(account.name, moneyWon)
-
-    return
+        db.UpdateMoney(account.name, moneyWon)
+        
+        selection = int(input("1 to gamble and 2 to exit"))
+        if selection == 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+        if selection == 2:
+            return
