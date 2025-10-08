1e38fe799b2464ceefed707be1340c12fbbf0060
Miko Gronroos <miko.gronroos@gmail.com>
2025-10-08 23:16:26 +0000

    Start work on selecting travel destination

--- Scripts/Core/travel.py
+++ Scripts/Core/travel.py
@@ -1,2 +1,9 @@
+import os
+
 def travel():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Select location to travel to!\n1. Bangkok\n2. Berlin")
+    selection = int(input("select airport 1-5 or exit 6: "))
+    if selection == 6:
+        return
     return
